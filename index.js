9d1c755d5f670ca878b221797047a41461baf652
dhruv2590 <dhruvpanwar25@gmail.com>
2024-02-28 20:39:28 +0500

    Inital Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let ele = document.getElementById("wrapper");
+// console.log(ele);
+ele.style.color = "red";
+ele.style.backgroundColor = "grey";
+ele.style.margin = "20px";
+ele.style.padding = "10px";
+ele.style.fontSize = "18px";
+ele.style.fontWeight = "bold";
+ele.style.height = "200px"
+ele.style.width = "300px";
